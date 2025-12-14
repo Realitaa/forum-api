f71cf4323dcb22ec39c62e212bd7d661a2e9520a
Realitaa <rezamuliaputra927@gmail.com>
2025-12-14 21:28:45 +0700

    feat: create POST commets API

---
+++ src/Interfaces/http/api/comments/handler.js
@@ -0,0 +1,32 @@
+class CommentsHandler {
+  constructor(container) {
+    this._container = container;
+
+    this.postCommentHandler = this.postCommentHandler.bind(this);
+  }
+
+  async postCommentHandler(request, h) {
+    const { id: userId } = request.auth.credentials;
+    const { threadId } = request.params;
+    const { content } = request.payload;
+
+    const addCommentUseCase = this._container.getInstance('AddCommentUseCase');
+
+    const addedComment = await addCommentUseCase.execute({
+      content,
+      owner: userId,
+      threadId,
+    });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedComment,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = CommentsHandler;
