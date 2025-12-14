0c0255942ebf1959648bfc0eda96e12880bbc785
Realitaa <rezamuliaputra927@gmail.com>
2025-12-14 22:46:41 +0700

    feat: add verifyCommentOwner and deleteComment in Domains CommentRepository

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -3,6 +3,14 @@ class CommentRepository {
   async addComment(thread) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async verifyCommentOwner(commentId, owner) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteComment(commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = CommentRepository;
