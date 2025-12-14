54fcbc422b803d68a90327a4b51e032ecbce81b3
Realitaa <rezamuliaputra927@gmail.com>
2025-12-14 18:15:46 +0700

    feat: create thread api

---
+++ src/Interfaces/http/api/threads/handler.js
@@ -0,0 +1,31 @@
+class ThreadsHandler {
+  constructor(container) {
+    this._container = container;
+
+    this.postThreadHandler = this.postThreadHandler.bind(this);
+  }
+
+  async postThreadHandler(request, h) {
+    const { id: userId } = request.auth.credentials;
+    const { title, body } = request.payload;
+
+    const addThreadUseCase = this._container.getInstance('AddThreadUseCase');
+
+    const addedThread = await addThreadUseCase.execute({
+      title,
+      body,
+      owner: userId,
+    });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedThread,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ThreadsHandler;
